f0464b0839751948f5bedab8b5186d266eb3aab9
Jonah Latham <jonah.latham21@gmail.com>
2019-06-29 12:08:00 -0600

    kata challenges

---
+++ javascript/sandBoxing.js
@@ -0,0 +1,46 @@
+// let string1 = 'Hello World'
+// let string2 = ''
+// let string3 = ' '
+
+// function isStringEmpty (str) {
+//     if (str.length <= 0){
+//         return 'the string is empty'
+//     } else {
+//         return 'the string has crap in it'
+//     }
+// }
+// console.log(isStringEmpty(string2))
+// console.log(string2.length)
+
+///////////////////////////////////////////////
+
+// let arr1 = [1,2,3,4,5]
+// let arr2 = [6,7,8,9,10]
+// function concatingArrays(array1, array2) {
+//     return array1.concat(array2)
+// }
+
+// console.log(concatingArrays(arr1, arr2))
+
+/////////////////////////////////////////////
+
+// let string = 'Jonah'
+// nameGreetings = (str) => {
+//     return `${str} greetings`
+// }
+// console.log(nameGreetings(string))
+
+/////////////////////////////////////////////
+
+// let string1 = 'cat'
+// let string2 = 'cats'
+// function isPlural (str) {
+//     if (str.charAt(str.length - 1) === 's'){
+//         return 'It is plural'
+//     } else {
+//         return 'It is not pluarl'
+//     }
+// }
+// console.log(isPlural(string2))
+
+//////////////////////////////////////////////
