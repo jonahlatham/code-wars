9c47dbd94bc5078ed757cd33ab709e549f16d044
Jonah Latham <jonah.latham21@gmail.com>
2019-06-30 21:42:04 -0600

    kata challenge

---
+++ javascript/8 Kata/NewCodeWars/howManyLightsabers.js
@@ -0,0 +1,11 @@
+let one = 'Zach'
+let two = 'anyone else'
+function howManyLightsabersDoYouOwn(name) {
+    if(name==='Zach'){
+        return 18
+    } else {
+        return 0
+    }
+  }
+
+  console.log(howManyLightsabersDoYouOwn(one))
