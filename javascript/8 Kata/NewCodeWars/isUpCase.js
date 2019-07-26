91927eaf5d721ac045c3c3cd8f2d7484ff4103f2
Jonah Latham <jonah.latham21@gmail.com>
2019-07-25 21:36:04 -0600

    kata

---
+++ javascript/8 Kata/NewCodeWars/isUpCase.js
@@ -0,0 +1,10 @@
+let strss = 'HELLO'
+function isItUpCase(str) {
+    if(str.toUpperCase() === str){
+        return true
+    } else {
+        return false
+    }
+  }
+
+  console.log(isItUpCase(strss))
