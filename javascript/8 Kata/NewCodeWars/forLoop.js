58a5abb37efa2986dba140ec7695ab40cf675110
Jonah Latham <jonah.latham21@gmail.com>
2019-07-30 21:59:09 -0600

    code wars

--- javascript/8 Kata/NewCodeWars/forLoop.js
+++ javascript/8 Kata/NewCodeWars/forLoop.js
@@ -2,14 +2,32 @@
 
 function pickIt(arr){
     var odd=[],even=[];
-    arr.map((e,i)=>{
-        if(e%2===0){
-            even.push(e)
+    for(let i=0; i<=arr.length; i++){
+        if(arr[i]%2===0){
+            even.push(arr[i])
+        } else if(arr[i]===undefined){
+            return [odd, even]
         } else {
-            odd.push(e)
+            odd.push(arr[i])
         }
-    })
+    }
     return [odd,even];
   }
+  console.log(pickIt([1,2,3,54,6,54,3,77,45,434,4321,887]))
 
-  console.log(pickIt([1,2,3]))
+  //why we use map
+
+//   function pickIt2(array){
+//     let odd=[]
+//     let even=[]
+//     array.map((e,i)=>{
+//           if(e%2===0){
+//             even.push(e)
+//           } else {
+//               odd.push(e)
+//           }
+//       })
+//       return [odd,even]
+//   }
+
+//   console.log(pickIt2([1,4,5,6,7,8,9,76,15,434,45,87]))
