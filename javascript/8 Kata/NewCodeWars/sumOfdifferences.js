2aff35dfbe4e51260244abee4b9558072127d09c
Jonah Latham <jonah.latham21@gmail.com>
2019-08-01 08:38:09 -0600

    kata

--- javascript/8 Kata/NewCodeWars/sumOfdifferences.js
+++ javascript/8 Kata/NewCodeWars/sumOfdifferences.js
@@ -14,12 +14,15 @@
 // }
 
 // console.log(sumOfDifferences(arr1, arr2))
+let array = [10,4,2,1]
+console.log((10-6)+(6-3)+(3-1))
 
+function sumOfDifferences(arr) {
+    let sorted = arr.sort((a,b)=>{return b-a})
+    if(sorted.lenght<=0){
+        return 0
+    }
+    return sorted[0]-sorted[sorted.length-1]
+}
 
-// function sumOfDifferences(arr) {
-//     return arr.sort((a,b)=>{return b-a}).reduce((r,e,i,a)=>{
-//         return r-=e
-//     }, 0)
-// }
-
-// console.log(sumOfDifferences([1,2,10]))
+console.log(sumOfDifferences([1,2,10]))
