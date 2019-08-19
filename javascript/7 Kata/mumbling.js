66d1aea0e4c3e4da48e46412f17b080acedbbd33
Jonah Latham <jonah.latham21@gmail.com>
2019-08-18 19:42:12 -0600

    kata

--- javascript/7 Kata/mumbling.js
+++ javascript/7 Kata/mumbling.js
@@ -1,12 +1,19 @@
 // https://www.codewars.com/kata/mumbling/train/javascript
 
 let string = 'abcd'
+// a-bb-ccc-dddd
+// b-aa-ccc-yyyy
 
 function accum(s) {
-let str = s.split('')
-return str.reduce((r,e,i,a)=>{
-    return r += e + str.length
-},'')
+    let str = s.split('')
+    return str.reduce((r, e, i, a) => {
+        if(i===str.length-1){
+            r += e.toUpperCase() + e.toLowerCase().repeat(i)
+        } else {
+            r += e.toUpperCase() + e.toLowerCase().repeat(i) +'-'
+        }
+        return r
+    }, '')
 }
 
 console.log(accum(string))
