61def063df1f7eeac456d7cc662576aafa64eae7
Jonah Latham <jonah.latham21@gmail.com>
2019-08-26 23:29:49 -0400

    kata

--- javascript/7 Kata/stringEndsWith.js
+++ javascript/7 Kata/stringEndsWith.js
@@ -2,15 +2,19 @@
 let string = 'cab'
 let stn = 'abc'
 
-function solution(str, ending) {
-    let strine = str.split('').map((e, i) => {
-        return e
-    })
-    if (strine.slice(strine.length - ending.length, strine.length).join('') === ending) {
-        return true
-    } else {
-        return false
-    }
-}
+// function solution(str, ending) {
+//     let strine = str.split('').map((e, i) => {
+//         return e
+//     })
+//     if (strine.slice(strine.length - ending.length, strine.length).join('') === ending) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
 
 console.log(solution(string, 'ab'))
