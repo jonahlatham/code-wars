a09bb7378abaf447b51c935e659afdc25ef7a16e
Jonah Latham <jonah.latham21@gmail.com>
2019-09-18 09:21:53 -0600

    code

---
+++ javascript/8 Kata/NewCodeWars/golfCode.js
@@ -0,0 +1,15 @@
+
+let str = '  hello world 345  my na me is jonah'
+function golfScore(code) {
+    let filtCode = code.trim().split(' ').filter((e, i) => {
+        if (/[a-z]/.test(e.toLowerCase()) || /[0-9]/.test(e.toLowerCase()) || e !== '') {
+            return true
+        } else {
+            return false
+        }
+    })
+    console.log(filtCode)
+    return filtCode.length
+}
+
+console.log(golfScore(str))
