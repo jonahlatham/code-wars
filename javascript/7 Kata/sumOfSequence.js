1f26816b4a46be4232452e027b4511e3035708bf
Jonah Latham <jonah.latham21@gmail.com>
2019-09-20 20:46:29 -0600

    crap

---
+++ javascript/7 Kata/sumOfSequence.js
@@ -0,0 +1,13 @@
+const sequenceSum = (begin, end, step) => {
+    let arr = []
+    for (let i = begin; i <= end; i++) {
+        arr.push(i * step)
+    }
+    return arr.reduce((r, e) => {
+        r += (e - step)
+        console.log(e)
+        return r
+    }, 0)
+};
+
+console.log(sequenceSum(1, 10, 2))
---
+++ javascript/7 Kata/twoToOne.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/two-to-one/train/javascript
+
+let a = 'abcdefga'
+let b = 'abcdefghijklmnop'
+
+function longest(s1, s2) {
+    // return s1.split('').filter((e,i)=>{
+    //     if(e.includes(s2.split(''))){
+    //         return false
+    //     } else {
+    //         return true
+    //     }
+    // })
+}
+
+console.log(longest(a, b))
