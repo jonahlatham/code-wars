dc743da0b0c333571867e1ffae6865e51a12bda1
Jonah Latham <jonah.latham21@gmail.com>
2019-09-23 12:00:00 +0100

    whatever

--- javascript/7 Kata/alphabeticalAddition.js
+++ javascript/7 Kata/alphabeticalAddition.js
@@ -1,10 +1,17 @@
 
 function addLetters(...letters) {
-
-    let newLet = letters.map((e,i,a)=>{
-           return String.fromCharCode(97 + String.fromCharCode(e));
-    })
-    return newLet
+    let alph = 'abcdefghijklmnopqrstuvwxyz'
+    const arr = alph.split('')
+    let reducer = arr.reduce((r, e, i) => {
+        if (letters.sort().includes(e)) {
+            r += (i + 1)
+        }
+        return r
+    }, 0)
+    while (reducer > 26) {
+        reducer -= 26
+    }
+    return letters[0] === undefined ? 'z' : arr[reducer - 1]
 }
 
-console.log(addLetters('a', 'b', 'c'))
+console.log(addLetters('a', 'z', 'd'))
---
+++ javascript/8 Kata/NewCodeWars/takeFirstNElements.js
@@ -0,0 +1,9 @@
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function take(arr, n) {
+    return arr.slice(0, n)
+}
+
+console.log(take(array, 4))
